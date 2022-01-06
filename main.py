9808c8d1979e366f0b4586c6a851b64bd5f0e16c
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-01-06 19:19:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+class Button():
+  def __init__():
+    pass
+  def isClicked():
+    pass
+    
+def drawGrid(): #Draw Sudoku Frame
+  pass
+
+keypad = []
+run = True
+
+while run:
+  #DRAW
+  #CHECK USER INPUT
+  #CHANGE VARIOUS STATES
