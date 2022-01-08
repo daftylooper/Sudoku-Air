91b3110c2a3f81a9d8fb07861d70ea7904a7d1bf
Prajwal shenoy <92460091+Prajwal-creator007@users.noreply.github.com>
2022-01-08 16:04:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ def drawGrid():
 
     blank =np.zeros((1920,1080),dtype='uint8')
     cv.imshow('Blank',blank)
-    cv.rectangle(blank, (0,0), (900,900), (255,255,255), thickness=4)
+    cv.rectangle(blank, (0,0), (900,900), (255,255,255), thickness=4)#To the the rectangle 
     cv.imshow('Rectangle',blank)
     for i in range(1,3):
-        cv.rectangle(blank, (0,0), (i*300,900), (255,255,255), thickness=3)
+        cv.rectangle(blank, (0,0), (i*300,900), (255,255,255), thickness=3)#To draw the bigger 9X9 layout 
         cv.imshow('Rectangle',blank)
     for i in range(1,3):
-        cv.rectangle(blank, (0,0), (900,i*300), (255,255,255), thickness=3)
+        cv.rectangle(blank, (0,0), (900,i*300), (255,255,255), thickness=3)#To draw the bigger 9X9 layout
         cv.imshow('Rectangle',blank)
     for i in range(1,9): 
-      cv.line(blank, (i*100,0), (i*100,900), (255,255,255), thickness=2)
+      cv.line(blank, (i*100,0), (i*100,900), (255,255,255), thickness=2)#To draw the inner 9X9 layout 
       cv.imshow('Line',blank)
     for i in range(1,9):
-      cv.line(blank, (0,i*100), (900,i*100), (255,255,255), thickness=2)
+      cv.line(blank, (0,i*100), (900,i*100), (255,255,255), thickness=2)#To draw the inner 9X9 layout 
       cv.imshow('Line',blank)
     
     cv.waitKey(0)
