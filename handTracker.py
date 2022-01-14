92b47e6cdf873800102f22a3b2777e77559b0b49
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-01-14 16:19:34 +0530

    modularize hand tracking code

--- handTracker.py
+++ handTracker.py
@@ -2,60 +2,66 @@ import cv2 as cv
 import time
 import mediapipe as mp
 import math
+import numpy
 
-#OS keeps switching the index of cameras, this finds the right one.
-capture = cv.VideoCapture(0)
-mpHands=  mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+class handTracking:
+    def __init__(self):
+        #OS keeps switching the index of cameras, this finds the right one.
+        self.capture = cv.VideoCapture(0)
+        isTrue, self.frame = self.capture.read()
+        print(type(self.frame))
+        if(not(isTrue)):
+            self.capture = cv.VideoCapture(1)
 
-pTime = 0
-cTime = 0
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
 
-def getLandmarkCor(handLandmarks, id=0): #Returns Tuple containing coordinates of specified landmark
-    Finger = handLandmarks.landmark[id]
-    height, width, channel = frame.shape
-    FingerCor = tuple((int(Finger.x*width), int(Finger.y*height)))
-    return FingerCor
+        self.pTime = 0
+        self.cTime = 0
 
+    def getCapture(self, width, height):
+        self.isTrue, self.frame = self.capture.read()
+        self.frame = cv.resize(cv.flip(self.frame, 1), (width, height), interpolation=cv.INTER_CUBIC)
+        rgbframe = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
+        self.result = self.hands.process(rgbframe)
 
-while True:
-        isTrue, frame = capture.read()
-        frame = cv.flip(frame, 1)
-        rgbframe = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        result = hands.process(rgbframe)
-        #print(result.multi_hand_landmarks)
+    def drawLandmark(self):
+        if self.result.multi_hand_landmarks:
+            for handLandmarks in self.result.multi_hand_landmarks: #Loop through ladnmark data that the model fetches
+                self.indexFingerCor = self.getLandmarkCor(handLandmarks, 8)
+                self.thumbFingerCor = self.getLandmarkCor(handLandmarks, 4)
+                self.middleFingerCor = self.getLandmarkCor(handLandmarks, 12)
 
-        if result.multi_hand_landmarks:
-            for handLandmarks in result.multi_hand_landmarks: #Loop through ladnmark data that the model fetches
-                indexFingerCor = getLandmarkCor(handLandmarks, 8)
-                thumbFingerCor = getLandmarkCor(handLandmarks, 4)
-                middleFingerCor = getLandmarkCor(handLandmarks, 12)
+                cv.line(self.frame, self.thumbFingerCor, self.middleFingerCor, (255, 0, 0), thickness=2)
+                cv.line(self.frame, self.thumbFingerCor, self.indexFingerCor, (255, 0, 0), thickness=2)
+                cv.line(self.frame, self.indexFingerCor, self.middleFingerCor, (255, 0, 0), thickness=2)
 
-                cv.line(frame, thumbFingerCor, middleFingerCor, (255, 0, 0), thickness=2)
-                cv.line(frame, thumbFingerCor, indexFingerCor, (255, 0, 0), thickness=2)
-                cv.line(frame, indexFingerCor, middleFingerCor, (255, 0, 0), thickness=2)
+                cv.circle(self.frame, self.indexFingerCor, 15, (0,255,255), thickness=-1)
 
-                dist = ((middleFingerCor[0]-thumbFingerCor[0])*(thumbFingerCor[1]-indexFingerCor[1]) - (thumbFingerCor[0]-indexFingerCor[0])*(middleFingerCor[1]-thumbFingerCor[1]))/math.hypot(middleFingerCor[0]-thumbFingerCor[0], middleFingerCor[1]-thumbFingerCor[1])
-                if(dist<=0):
-                    print("Click Found")
-                #base = tuple(((thumbFingerCor[0]+middleFingerCor[0])/2, (middleFingerCor[1]+middleFingerCor[1])/2))
+                self.mpDraw.draw_landmarks(self.frame, handLandmarks, self.mpHands.HAND_CONNECTIONS)
 
-                cv.circle(frame, indexFingerCor, 15, (0,255,255), thickness=-1)
+    def getLandmarkCor(self, handLandmarks, id=0): #Returns Tuple containing coordinates of specified landmark
+        Finger = handLandmarks.landmark[id]
+        height, width, channel = self.frame.shape
+        FingerCor = tuple((int(Finger.x*width), int(Finger.y*height)))
+        return FingerCor
 
-                mpDraw.draw_landmarks(frame, handLandmarks, mpHands.HAND_CONNECTIONS)
+    def ifClicked(self):
+        dist = ((self.middleFingerCor[0]-self.thumbFingerCor[0])*(self.thumbFingerCor[1]-self.indexFingerCor[1]) - (self.thumbFingerCor[0]-self.indexFingerCor[0])*(self.middleFingerCor[1]-self.thumbFingerCor[1]))/math.hypot(self.middleFingerCor[0]-self.thumbFingerCor[0], self.middleFingerCor[1]-self.thumbFingerCor[1])
+        if(dist<=0): #  For now 0 is the threshold, it can even be a +ve num so the user doesn't have to bend finger too low
+            return True
+        return False
 
-        cTime = time.time()
-        fps = 1/(cTime-pTime)
-        pTime = cTime
+    def returnProcessedVideoFeed(self):
+        self.cTime = time.time()
+        fps = 1/(self.cTime-self.pTime)
+        pTime = self.cTime
 
-        cv.putText(frame, str(int(fps)), (10, 70), cv.FONT_ITALIC, 3, (0, 0, 255), 3)
+        cv.putText(self.frame, str(int(fps)), (10, 70), cv.FONT_ITALIC, 3, (0, 0, 255), 3)
 
-        if(isTrue):
-            cv.imshow('Video', frame)
+        return self.frame
 
-        if cv.waitKey(20) & 0xFF==ord('d'):
-            break
-
-capture.release()
-cv.destroyAllWindows()
+    def stopRender(self):
+        self.capture.release()
+        cv.destroyAllWindows()
