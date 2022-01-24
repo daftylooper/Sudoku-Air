1e25ca0ee9bb8f1ef694b1be1e7de74f6cb2085c
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-01-24 23:33:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,26 +7,35 @@ class Button():
         self.pos = pos
         self.size = size
         self.text = text
+        self.highlight = False
     
     def draw(self):
         x,y= self.pos
-        w,h= self.size 
-        cv.rectangle(self.root, self.pos, (x+w, y+h), (255,255,255),2)
-        cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (255,255,255), 4)
+        w,h= self.size
+        if(self.highlight):
+            cv.rectangle(self.root, self.pos, (x+w, y+h), (255,0,0), -1)
+            cv.rectangle(self.root, self.pos, (x+w, y+h), (255,255,255), -1)
+            cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (255,255,255), 4)
+        else:
+            cv.rectangle(self.root, self.pos, (x+w, y+h), (255,255,255), 2)
+            cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (255,255,255), 4)
         
-    def withinButton(self, xcor, ycor):
-        if((xcor>=self.pos.x and xcor<=self.pos.x+self.size.w) and (ycor>=self.pos.y and ycor<=self.pos.y+self.size.h)):
-            return True
-        return False
+    def withinButton(self, cor, clicked):
+        if(clicked):
+            if((cor[0]>=self.pos[0] and cor[0]<=self.pos[0]+self.size[0]) and (cor[1]>=self.pos[1] and cor[1]<=self.pos[1]+self.size[1])):
+                return True
+            return False
+
+    def highlightButton(self):
+        self.highlight = True
 
 class Grid():    
-    def drawGrid(blank, x, y):
-        cv.rectangle(blank, (0,0), (x, y), (255,255,255), thickness=4)
+    def drawGrid(blank, x, y, xshift, yshift): #x, y - width, height <===> xshift, yshift - x, y
+        cv.rectangle(blank, (xshift, yshift), (x+xshift, y+yshift), (255,255,255), thickness=4)
         for i in range(1,3):
-            cv.rectangle(blank, (0,0), (i*int(x/3), y), (255,255,255), thickness=3)
-        for i in range(1,3):
-            cv.rectangle(blank, (0,0), (x, i*int(y/3)), (255,255,255), thickness=3)
+            cv.line(blank, (xshift+i*int(x/3), yshift), (xshift+i*int(x/3), y+yshift), (255,255,255), thickness=3)#Vertical Line
+            cv.line(blank, (xshift, yshift+i*int(y/3)), (x+xshift, yshift+i*int(y/3)), (255,255,255), thickness=3)#Horizontal Line
         for i in range(1,9):
-            cv.line(blank, (i*int(x/9),0), (i*int(x/9), y), (255,255,255), thickness=2)
+            cv.line(blank, (i*int(x/9)+xshift, yshift), (i*int(x/9)+xshift, y+yshift), (255,255,255), thickness=2)
         for i in range(1,9):
-            cv.line(blank, (0,i*int(y/9)), (x, i*int(y/9)), (255,255,255), thickness=2)
+            cv.line(blank, (xshift, i*int(y/9)+yshift), (x+xshift, i*int(y/9)+yshift), (255,255,255), thickness=2)
