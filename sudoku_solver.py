68bef31062cadb9254f44e9e3f13e3f852eb4c8a
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-01-24 23:32:42 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,21 +1,10 @@
 import numpy as np
 import pickle
 
-grid = [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,0,1,9,0,0,5],
-        [0,0,0,0,0,0,0,0,0]]
-
 class solver():
-    def __init__(self, someGrid):
+    def __init__(self):
         self.solutions = []
-        self.grid = someGrid
-        self.x = 0
+        self.grid = []
 
     def possible(self, row, column, number):
         #Is the number appearing in the given row?
@@ -23,12 +12,10 @@ class solver():
             if self.grid[row][i] == number:
                 return False
 
-        #Is the number appearing in the given column?
         for i in range(0,9):
             if self.grid[i][column] == number:
                 return False
         
-        #Is the number appearing in the given square?
         x0 = (column // 3) * 3
         y0 = (row // 3) * 3
         for i in range(0,3):
@@ -43,18 +30,17 @@ class solver():
             for column in range(0,9):
                 if self.grid[row][column] == 0:
                     for number in range(1,10):
-                        if self.possible(row, column, number):
+                        if self.possible(row, column, number): 
                             self.grid[row][column] = number
                             self.solve()
                             self.grid[row][column] = 0
-
+   
                     return
-    
-        self.solutions.append(self.grid)
 
-    def getSolutions(self):
-        return self.solutions
+        self.solutions.append([row.copy() for row in self.grid])
 
+'''
 solvo = solver(grid)
 solvo.solve()
-print(solvo.getSolutions())
+print(np.matrix(sol[0]))
+'''
