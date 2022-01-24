c1c9785a9ad9489a9f8621740c121cfe81f4ba2d
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-01-24 23:31:44 +0530

    Update handTracker.py

--- handTracker.py
+++ handTracker.py
@@ -6,15 +6,17 @@ import numpy
 
 class handTracking:
     def __init__(self):
-        #OS keeps switching the index of cameras, this finds the right one
+        #OS keeps switching the index of cameras, this finds the right one.
         self.capture = cv.VideoCapture(0)
         isTrue, self.frame = self.capture.read()
+        print(type(self.frame))
         if(not(isTrue)):
             self.capture = cv.VideoCapture(1)
 
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands()
         self.mpDraw = mp.solutions.drawing_utils
+        self.coords = tuple((0, 0))
 
         self.pTime = 0
         self.cTime = 0
@@ -22,13 +24,14 @@ class handTracking:
     def getCapture(self, width, height):
         self.isTrue, self.frame = self.capture.read()
         self.frame = cv.resize(cv.flip(self.frame, 1), (width, height), interpolation=cv.INTER_CUBIC)
-        rgbframe = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
-        self.result = self.hands.process(rgbframe)
+        self.rgbframe = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
 
     def drawLandmark(self):
+        self.result = self.hands.process(self.rgbframe)
         if self.result.multi_hand_landmarks:
             for handLandmarks in self.result.multi_hand_landmarks: #Loop through ladnmark data that the model fetches
                 self.indexFingerCor = self.getLandmarkCor(handLandmarks, 8)
+                self.coords = self.indexFingerCor
                 self.thumbFingerCor = self.getLandmarkCor(handLandmarks, 4)
                 self.middleFingerCor = self.getLandmarkCor(handLandmarks, 12)
 
@@ -40,25 +43,28 @@ class handTracking:
 
                 self.mpDraw.draw_landmarks(self.frame, handLandmarks, self.mpHands.HAND_CONNECTIONS)
 
+        self.cTime = time.time()
+        fps = 1/(self.cTime-self.pTime)
+        pTime = self.cTime
+
+        cv.putText(self.frame, str(int(fps)), (20, 625), cv.FONT_ITALIC, 3, (0, 0, 255), 3)
+
     def getLandmarkCor(self, handLandmarks, id=0): #Returns Tuple containing coordinates of specified landmark
         Finger = handLandmarks.landmark[id]
         height, width, channel = self.frame.shape
         FingerCor = tuple((int(Finger.x*width), int(Finger.y*height)))
         return FingerCor
 
+    def getCoords(self):
+        return self.coords
+
     def ifClicked(self):
         dist = ((self.middleFingerCor[0]-self.thumbFingerCor[0])*(self.thumbFingerCor[1]-self.indexFingerCor[1]) - (self.thumbFingerCor[0]-self.indexFingerCor[0])*(self.middleFingerCor[1]-self.thumbFingerCor[1]))/math.hypot(self.middleFingerCor[0]-self.thumbFingerCor[0], self.middleFingerCor[1]-self.thumbFingerCor[1])
         if(dist<=0): #  For now 0 is the threshold, it can even be a +ve num so the user doesn't have to bend finger too low
-            return True
-        return False
+            return tuple((self.indexFingerCor[0], self.indexFingerCor[1])), True
+        return tuple((0, 0)), False
 
     def returnProcessedVideoFeed(self):
-        self.cTime = time.time()
-        fps = 1/(self.cTime-self.pTime)
-        pTime = self.cTime
-
-        cv.putText(self.frame, str(int(fps)), (10, 70), cv.FONT_ITALIC, 3, (0, 0, 255), 3)
-
         return self.frame
 
     def stopRender(self):
