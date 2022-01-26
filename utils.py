b185ef5c34e8351fdd9572adeb243b39816e031b
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-01-26 21:55:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,33 +1,31 @@
 import cv2 as cv
 import numpy as np
+import pickle
+import os
 
 class Button():
-    def  __init__(self, root, pos, text, size=[70,70]):
+    def  __init__(self, root, pos, text, size=[70,70], highlight=False):
         self.root = root
         self.pos = pos
         self.size = size
         self.text = text
-        self.highlight = False
+        self.highlight = highlight
     
     def draw(self):
-        x,y= self.pos
-        w,h= self.size
+        x,y = self.pos
+        w,h = self.size
         if(self.highlight):
-            cv.rectangle(self.root, self.pos, (x+w, y+h), (255,0,0), -1)
-            cv.rectangle(self.root, self.pos, (x+w, y+h), (255,255,255), -1)
-            cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (255,255,255), 4)
+            cv.rectangle(self.root, self.pos, (x+w, y+h), (255, 255, 255), -1)
+            cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
         else:
-            cv.rectangle(self.root, self.pos, (x+w, y+h), (255,255,255), 2)
-            cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (255,255,255), 4)
+            cv.rectangle(self.root, self.pos, (x+w, y+h), (0, 0, 0), 2)
+            cv.putText(self.root, self.text, (x+20, y+50), cv.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 4)
         
     def withinButton(self, cor, clicked):
         if(clicked):
             if((cor[0]>=self.pos[0] and cor[0]<=self.pos[0]+self.size[0]) and (cor[1]>=self.pos[1] and cor[1]<=self.pos[1]+self.size[1])):
                 return True
             return False
-
-    def highlightButton(self):
-        self.highlight = True
 
 class Grid():    
     def drawGrid(blank, x, y, xshift, yshift): #x, y - width, height <===> xshift, yshift - x, y
@@ -39,3 +37,25 @@ class Grid():
             cv.line(blank, (i*int(x/9)+xshift, yshift), (i*int(x/9)+xshift, y+yshift), (255,255,255), thickness=2)
         for i in range(1,9):
             cv.line(blank, (xshift, i*int(y/9)+yshift), (x+xshift, i*int(y/9)+yshift), (255,255,255), thickness=2)
+
+class pickler():
+    def __init__(self, topickle):
+        self.puzzles = topickle
+    def pickle(self):
+        if(os.path.exists("sudoku_puzzles.txt")):
+            os.remove("sudoku_puzzles.txt")
+        outfile = open("sudoku_puzzles.txt", 'ab')
+        pickle.dump(self.puzzles, outfile)
+        outfile.close()
+    def unpickle(self):
+        outfile = open("sudoku_puzzles", 'rb')
+        self.puzzles = pickle.load(outfile)
+        outfile.close()
+    def addpuzzle(self, sudoku):
+        self.unpickle()
+        self.puzzles.append(sudoku)
+        self.pickle()
+    def removepuzzle(self, index):
+        self.unpickle()
+        self.puzzles.remove(index)
+        self.pickle()
